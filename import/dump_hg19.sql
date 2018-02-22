75b565d75c0e5cd809c70025f117a930a5c4fbe6
frankyan <frankphy@gmail.com>
2018-02-22 11:16:18 -0800

    correct hg19.sql

--- import/dump_hg19.sql
+++ import/dump_hg19.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2e1e43af5d837cceec615d87780f55f7472d560219facf28feeebf2633e57967
-size 116941254
+oid sha256:86ee54e74166b86f39d1bc32e2779bf6f7bd9abb0fdb521178f2a3cb13569a1e
+size 116941199
